6bc08de5bd8ef9d0d7eec1d4c2e49dc71967e7c9
Michael Burke <michael_burke4@student.uml.edu>
2024-03-29 18:54:20 -0400

    finding words in word list

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <err.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +17,37 @@ static int get_value(char c) {
 	if (c < 'a' || c > 'z')
 		return -1;
 	return letter_values[(int)(c - 'a')];
+}
+
+
+static bool is_word(char *word)
+{
+	size_t lb = 0;
+	size_t ub = (sizeof words) / (sizeof (char *)) - 1;
+	size_t mp;
+	int cmp_res = -1;
+	while (lb <= ub) {
+		mp = lb + ((ub - lb) / 2);
+		cmp_res = strcmp(word, words[mp]);
+		if (!cmp_res)
+			return true;
+		else if (cmp_res > 0)
+			lb = mp + 1;
+		else
+			ub = mp - 1;
+	}
+	return false;
+}
+
+static ssize_t find_first_prefix_index(char *word) {
+	size_t i = 0;
+	size_t len = strlen(word);
+	int res;
+	while ((res = strncmp(word, words[i], len)) > 0)
+		++i;
+	if (!res)
+		return i;
+	return -1;
 }
 
 typedef struct {
@@ -38,5 +70,6 @@ tile build_tile(char letter) {
 }
 
 int main(void) {
+	printf("%ld\n", find_first_prefix_index("syz"));
 	return 0;
 }
