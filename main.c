93d12af1721dca4ecb0c029e3b35199e5c9d7f99
Michael Burke <michael_burke4@student.uml.edu>
2024-03-30 19:49:38 -0400

    user friendly input

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <err.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -58,7 +59,7 @@ static ssize_t find_first_prefix_index(char *word, size_t start) {
 typedef struct {
 	char letter;
 	bool double_word;
-	unsigned int letter_mult;
+	int letter_mult;
 	bool used;
 } tile;
 
@@ -78,10 +79,30 @@ static void init_board(char str[BOARD_SIZE * BOARD_SIZE + 1]) {
 	}
 }
 
+#define RED   "\x1B[31m"
+#define YEL   "\x1B[33m"
+#define MAG   "\x1B[35m"
+#define RESET "\x1B[0m"
+
 static void print_board() {
+	int color_flag = 0;
 	for (int i = 0 ; i < BOARD_SIZE ; ++i) {
 		for (int j = 0 ; j < BOARD_SIZE ; ++j) {
+			if (board[i][j].letter_mult == 2) {
+				printf(YEL);
+				color_flag = 1;
+			} else if (board[i][j].letter_mult == 3) {
+				printf(RED);
+				color_flag = 1;
+			} else if (board[i][j].double_word) {
+				printf(MAG);
+				color_flag = 1;
+			}
 			printf("%c ", board[i][j].letter);
+			if (color_flag) {
+				color_flag = 0;
+				printf(RESET);
+			}
 		}
 		printf("\n");
 	}
@@ -169,11 +190,104 @@ static void find_all_words() {
 	}
 }
 
+static void get_row(char **row_ptr) {
+	size_t len;
+	size_t nread;
+	while ((nread = getline(row_ptr, &len, stdin)) != (BOARD_SIZE + 1)) {
+		printf("improper row size: must be %d chars long\n", BOARD_SIZE);
+	}
+	(*row_ptr)[5] = '\0';
+}
+
+static long read_int() {
+	size_t len;
+	size_t nread;
+	long val;
+	char *end;
+	char *in = 0;
+	while (1) {
+		nread = getline(&in, &len, stdin);
+		if (nread == -1) {
+			puts("Bad input, try again");
+		}
+		errno = 0;
+		val = strtol(in, &end, 10);
+		if (errno || end == in)
+			puts("Please enter a valid integer");
+		else {
+			free(in);
+			in = 0;
+			return val;
+		}
+	}
+}
+
 int main(void) {
-      //init_board("qqqqqqqqqqqqqqqqqqqqqqqqq");
-	init_board("nuugifcisgmgpbjdoqtidnwia");
-	board[2][0].letter_mult = 2;
-	board[2][1].double_word = true;
-	find_all_words();
-	return 0;
+	char *in = 0;
+	char boardstr[BOARD_SIZE*BOARD_SIZE+1];
+	boardstr[0] = 0;
+	long mult;
+	long mul_row;
+	long mul_col;
+	long dub_row;
+	long dub_col;
+
+	for (int i = 0 ; i < BOARD_SIZE ; ++i) {
+		printf("Input row %d\n", i);
+		get_row(&in);
+		strcat(boardstr, in);
+		free(in);
+		in = 0;
+	}
+	while (1) {
+		printf("What is the current letter multiplier (2 or 3)? ");
+		mult = read_int();
+		if (mult == 2 || mult == 3)
+			break;
+	}
+	while (1) {
+		printf("Which row is the %s letter in? ", mult == 2 ? "double" : "triple");
+		mul_row = read_int();
+		if (mul_row < 0 || mul_row >= BOARD_SIZE) {
+			puts("Value out of range. Try again.");
+			continue;
+		}
+		break;
+	}
+	while (1) {
+		printf("Which col is the %s letter in? ", mult == 2 ? "double" : "triple");
+		mul_col = read_int();
+		if (mul_col < 0 || mul_col >= BOARD_SIZE) {
+			puts("Value out of range. Try again.");
+			continue;
+		}
+		break;
+	}
+	while (1) {
+		printf("Which row is the double word in? (-1 if it isn't there yet) ");
+		dub_row = read_int();
+		if (dub_row == -1) {
+			break;
+		}
+		if (dub_row < 0 || dub_row >= BOARD_SIZE) {
+			puts("Value out of range. Try again.");
+			continue;
+		}
+		break;
+	}
+	while (dub_row != -1) {
+		printf("Which col is the double word in? ");
+		dub_col = read_int();
+		if (dub_col < 0 || dub_col >= BOARD_SIZE) {
+			puts("Value out of range. Try again.");
+			continue;
+		}
+		break;
+	}
+
+	init_board(boardstr);
+	board[mul_row][mul_col].letter_mult = (unsigned int)mult;
+	if (dub_row != -1)
+		board[dub_row][dub_col].double_word = true;
+	print_board();
 }
